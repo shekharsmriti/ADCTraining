4ef2b4ee51e4a080171c2e8d033a10d5a724317f
smshekha <smshekha@DIN56001175.corp.capgemini.com>
2017-08-09 14:10:32 +0530

    initial commit

---
+++ MobileBillingMaven/src/main/java/com/cg/mobilebilling/beans/Bill.java
@@ -0,0 +1,9 @@
+package com.cg.mobilebilling.beans;
+
+@
+public class Bill {
+	private int billID; 
+	private int noOfLocalSMS, noOfStdSMS, noOfLocalCalls, noOfStdCalls, internetDataUsageUnits;
+	private String billMonth;
+	private float totalBillAmount, localSMSAmount, stdSMSAmount, localCallAmount, stdCallAmount, internetDataUsageAmount, servicesTax, vat;
+}
